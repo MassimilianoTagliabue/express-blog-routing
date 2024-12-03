79020ed78884d493d3cc0e833f2927342dee57ab
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2024-12-03 18:33:36 +0100

    feat: routing

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
+const postRouter = require("./routers/posts")
+
+
+app.use("/posts", postRouter);
+
+app.use(express.static(`public`));
 
 
 app.get("/" , (req,res) => {
---
+++ routers/posts.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const postList = require("../data/posts");
+
+//index
+router.get("/", (req,res) => {
+
+    res.json({
+        data : postList,
+        total : postList.length
+    });
+})
+
+//show
+router.get("/:id", (req,res) =>{
+
+    const postid = req.params.id;
+    res.json("leggiamo l'elemento con id " +postid );
+
+});
+
+//create
+router.post("/" , (req,res) => {
+
+    res.json("aggiungo un nuovo elemento");
+})
+
+//update
+router.put("/:id" , (req,res) =>{
+
+    const postid = req.params.id;
+    res.json("modifico i dati dell'elemento con id " +postid);
+
+});
+
+//modify
+router.patch("/:id" , (req,res) => {
+
+    const postid = req.params.id;
+    res.json("modifichiamo gli specifici dati dell'elemento con id " +postid);
+});
+
+
+//destroy
+router.delete("/:id", (req,res) => {
+
+    const postid = req.params.id;
+    res.json("elimino l'elemento con id " +postid);
+});
+
+
+module.exports = router;
